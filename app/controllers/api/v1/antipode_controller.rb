1c3094a36c04494bcf09b7c9c20d8006ea92e1ce
Andrew Johnson <manilda7@gmail.com>
2019-11-04 10:13:38 -0700

    Adds further logic to AnitpodeController

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -11,10 +11,21 @@ class Api::V1::AntipodeController < ApplicationController
       f.params[:key] = ENV['GOOGLE_API_KEY']
     end
 
-  location_data = JSON.parse(response.body, symbolize_names: true)
-  lat = location_data[:results].first[:geometry][:location][:lat]
-  lng = location_data[:results].first[:geometry][:location][:lng]
-  response = Faraday.get("http://amypode.herokuapp.com/api/v1/antipodes?lat=#{lat}&long=#{lng}")
-  # binding.pry
+    location_data = JSON.parse(response.body, symbolize_names: true)
+    lat = location_data[:results].first[:geometry][:location][:lat]
+    lng = location_data[:results].first[:geometry][:location][:lng]
+
+    response = Faraday.get("http://amypode.herokuapp.com/api/v1/antipodes?lat=#{lat}&long=#{lng}") do |f|
+      f.headers['api_key'] = ENV['AMYPODE_API_KEY']
+    end
+
+    antipode_data = JSON.parse(response.body, symbolize_names: true)
+    anti_lat = antipode_data[:data][:attributes][:lat]
+    anti_long = antipode_data[:data][:attributes][:long]
+
+    dark_sky_response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARKSKY_API_KEY']}/#{anti_lat},#{anti_long}")
+    antipode_response = JSON.parse(dark_sky_response.body, symbolize_names: true)
+    
+  binding.pry
   end
 end
