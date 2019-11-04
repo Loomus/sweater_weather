aa4f7ef84d495bdd1fcb5c48c8d72f37398dd618
Andrew Johnson <manilda7@gmail.com>
2019-11-04 09:59:03 -0700

    Adds working through antipode logic

--- app/controllers/api/v1/antipode_controller.rb
+++ app/controllers/api/v1/antipode_controller.rb
@@ -1,9 +1,20 @@
 class Api::V1::AntipodeController < ApplicationController
   def index
+    location = params[:location]
+    # render json: AnitpodeSerializer.new(
+    #   AntipodeFacade.new(params[:location])
+    # )
+    # forecast_data = WeatherService.forecast_data(location)
     # binding.pry
-    # location = params[:location]
-    render json: AnitpodeSerializer.new(
-      AntipodeFacade.new(params[:location])
-    )
+    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json') do |f|
+      f.params[:address] = location
+      f.params[:key] = ENV['GOOGLE_API_KEY']
+    end
+
+  location_data = JSON.parse(response.body, symbolize_names: true)
+  lat = location_data[:results].first[:geometry][:location][:lat]
+  lng = location_data[:results].first[:geometry][:location][:lng]
+  response = Faraday.get("http://amypode.herokuapp.com/api/v1/antipodes?lat=#{lat}&long=#{lng}")
+  # binding.pry
   end
 end
--- app/facades/antipode_facade.rb
+++ app/facades/antipode_facade.rb
@@ -1,3 +1,3 @@
 class AntipodeFacade
-
-end 
+  
+end
